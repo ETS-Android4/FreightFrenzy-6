7de5e86e626d47822cc2b12535730110e2255446
Nakulj11 <nakulsaliljoshi@gmail.com>
2022-02-24 14:03:56 -0500

    Adjusted intakes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/IntakeAlternate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/component/IntakeAlternate.java
@@ -1,24 +1,24 @@
 package org.firstinspires.ftc.teamcode.component;
 
+
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Intake {
+public class IntakeAlternate {
     private CRServo spinner;
+    private Servo claw;
 
-
-
-
-    //tetrix: 1440 ticks per revolution
-    //andymark: 1120 ticks per rev
+    private final double CLOSE =  0.3;
+    private final double OPEN = 0;
 
     //init
     public void init(HardwareMap hardwareMap){
 
         spinner = hardwareMap.get(CRServo.class, "s1");
+        claw =  hardwareMap.get(Servo.class, "s2");
 
-
+        claw.setPosition(CLOSE);
 
     }
 
@@ -39,8 +39,15 @@ public class Intake {
     }
 
 
+    public void open(){
+        claw.setPosition(OPEN);
+    }
 
+    public void close(){
+        claw.setPosition(CLOSE);
+    }
 
 
 
 }
+
